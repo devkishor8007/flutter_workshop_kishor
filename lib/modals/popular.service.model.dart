c078e0b8b1f8b5ee1a079e42c33169f9dc910bd6
kishorkc <kishor8007kc@gmail.com>
2022-07-29 08:46:58 +0545

    add assets-models

---
+++ lib/modals/popular.service.model.dart
@@ -0,0 +1,64 @@
+// import 'package:flutter/material.dart';
+
+// class PopularServicesData {
+//   String title;
+//   String desc;
+//   String address;
+//   int price;
+//   Image image;
+//   double rating;
+//   int reviews;
+//   bool isBookmark;
+
+//   PopularServicesData(
+//       {required this.title,
+//       required this.desc,
+//       required this.image,
+//       required this.price,
+//       required this.address,
+//       required this.rating,
+//       required this.reviews,
+//       required this.isBookmark});
+// }
+
+// List<PopularServicesData> getPopularServicesData = [
+//   PopularServicesData(
+//     title: "Plumbing Service",
+//     desc: "Plumbing Service is ...",
+//     image: Image.asset(
+//       "assets/services/boy1.png",
+//       fit: BoxFit.cover,
+//     ),
+//     price: 50,
+//     address: 'Sanjuanita Ordonez',
+//     rating: 4.9,
+//     reviews: 80,
+//     isBookmark: false,
+//   ),
+//   PopularServicesData(
+//     title: "Car Repairing",
+//     desc: "Car Repairing is ...",
+//     image: Image.asset(
+//       "assets/services/boy2.png",
+//       fit: BoxFit.cover,
+//     ),
+//     price: 90,
+//     address: 'Wade Warren',
+//     rating: 4.9,
+//     reviews: 110,
+//     isBookmark: true,
+//   ),
+//   PopularServicesData(
+//     title: "Painting House",
+//     desc: "Painting House is ...",
+//     image: Image.asset(
+//       "assets/services/boy3.png",
+//       fit: BoxFit.cover,
+//     ),
+//     price: 50,
+//     address: 'Geoffrey Mott',
+//     rating: 3.9,
+//     reviews: 50,
+//     isBookmark: false,
+//   ),
+// ];
---
+++ lib/modals/ured.model.dart
@@ -0,0 +1,80 @@
+// import 'package:flutter/material.dart';
+
+// class UredServicesData {
+//   String title;
+//   String desc;
+//   Image image;
+
+//   UredServicesData({
+//     required this.title,
+//     required this.desc,
+//     required this.image,
+//   });
+// }
+
+// List<UredServicesData> getUredServicesData = [
+//   UredServicesData(
+//     title: "Plumbing",
+//     desc: "Plumbing is ...",
+//     image: Image.asset(
+//       "assets/ured/plumbing.png",
+//       fit: BoxFit.cover,
+//     ),
+//   ),
+//   UredServicesData(
+//     title: "Repairing",
+//     desc: "Repairing is ...",
+//     image: Image.asset(
+//       "assets/ured/repair.png",
+//       fit: BoxFit.cover,
+//     ),
+//   ),
+//   UredServicesData(
+//     title: "Electrical",
+//     desc: "Electrical is ...",
+//     image: Image.asset(
+//       "assets/ured/engineer.png",
+//       fit: BoxFit.cover,
+//     ),
+//   ),
+//   UredServicesData(
+//     title: "Cleaning",
+//     desc: "Cleaning is ...",
+//     image: Image.asset(
+//       "assets/ured/cleaning.png",
+//       fit: BoxFit.cover,
+//     ),
+//   ),
+//   UredServicesData(
+//     title: "Carpentry",
+//     desc: "Carpentry is ...",
+//     image: Image.asset(
+//       "assets/ured/carpenter.png",
+//       fit: BoxFit.cover,
+//     ),
+//   ),
+//   UredServicesData(
+//     title: "Painting",
+//     desc: "Painting is ...",
+//     image: Image.asset(
+//       "assets/ured/paint.png",
+//       fit: BoxFit.cover,
+//     ),
+//   ),
+//   UredServicesData(
+//     title: "Moving",
+//     desc: "Moving is ...",
+//     image: Image.asset(
+//       "assets/ured/moving.png",
+//       fit: BoxFit.cover,
+//     ),
+//   ),
+//   UredServicesData(
+//     title: "Handyman",
+//     desc: "Handyman is ...",
+//     image: Image.asset(
+//       "assets/ured/handyman.png",
+//       fit: BoxFit.cover,
+//     ),
+//   ),
+// ];
